ae025a0b2779cdbeb2bb96f4948125d5b8735d5b
karrim0 <karimhnfy1@gmail.com>
2026-05-26 03:41:22 +0300

    Update services APIs

--- src/Services/newsService.js
+++ src/Services/newsService.js
@@ -220,6 +220,17 @@ const newsService = {
 
     return response.data;
   },
+  getCollegesLogos: async ({ langId = 1, pageIndex = 1, pageSize = 100 }) => {
+  const response = await facultyApi.get("/faculty-news/GetCollegesLogos", {
+    params: {
+      langId,
+      PageIndex: pageIndex,
+      PageSize: pageSize,
+    },
+  });
+
+  return response.data;
+},
 };
 
 export default newsService;
